0ec69884b1af0a8e761909affd4d0e2e088c0879
Donnie Chen <Donnie.Chen@Quest.com>
2020-01-07 00:52:59 +0800

    1. 修改部分代码，以适配个人开发需求 2. 提交的代码内容仅作为测试使用

--- src/main/java/qdu/java/recruit/DemoApplication.java
+++ src/main/java/qdu/java/recruit/DemoApplication.java
@@ -3,9 +3,7 @@ package qdu.java.recruit;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -14,15 +12,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @ComponentScan
 @MapperScan("qdu.java.recruit.mapper")
 @EnableScheduling
-public class DemoApplication extends SpringBootServletInitializer {
+public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(DemoApplication.class);
-	}
-
+	
 }
